8e3e7fb1957fcf5a5c5952eea21e24cd7c7d8976
Logan Linn <logan.linn@gmail.com>
2011-04-06 23:05:14 -0400

    first-fit list moved to  nested class in memeory manager

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -21,6 +21,7 @@ public class MemoryManager {
 
 	/**
 	 * Creates the manager
+	 * 
 	 * @throws IOException
 	 */
 	public MemoryManager() throws IOException {
@@ -204,16 +205,206 @@ public class MemoryManager {
 
 	/**
 	 * Closes the random access file
+	 * 
 	 * @throws IOException
 	 */
 	public void close() throws IOException {
 		seqAccess.close();
 	}
-	
+
 	/**
 	 * A wrapper method for printing our free block
 	 */
-	public void printFreeBlocks(){
+	public void printFreeBlocks() {
 		firstFit.printFreeBlocks();
 	}
+
+	private class FirstFitList {
+		private LinkedList<FreeBlock> freeBlocks = new LinkedList<FreeBlock>();
+
+		public FirstFitList() {
+
+		}
+
+		public void releaseBlock(SequenceFileHandle handle) {
+			// POSSIBLE OUTCOMES
+			// 1) Last free block before handle can absorb the size of handle
+			// (when
+			// block.end == handle.offset)
+			// Otherwise, that means there's a block we shouldn't touch between
+			// free
+			// block and handle
+			// 2) Next free block can expand backwards (when handle.end ==
+			// block.offset)
+			// 3) Both (free blocks before and after can combine)
+			// 4) Handle just becomes a free block
+			// a) no surrounding free blocks
+			// b) first free block
+
+			// fast forward
+
+			int size = MemoryManager.getEncodedSequenceLength(handle
+					.getSequenceLength());
+			long offset = handle.getSequenceFileOffset();
+			long end = size + offset;
+			// System.out.println("  Deleting "+offset+"+"+size+"="+end);
+
+			/*
+			 * Find blocks that could be merged
+			 */
+			ListIterator<FreeBlock> blockIt = freeBlocks.listIterator();
+			FreeBlock prevBlock = null;
+			FreeBlock nextBlock = null;
+			while (blockIt.hasNext()) {
+				FreeBlock currBlock = blockIt.next();
+
+				if (currBlock.getEnd() == offset) {
+					// System.out.println("    Prev "+currBlock);
+					prevBlock = currBlock;
+
+					// Break if we have already found our next block
+					if (nextBlock != null) {
+						break;
+					}
+				} else if (currBlock.getOffset() == end) {
+					// System.out.println("    Next "+currBlock);
+					nextBlock = currBlock;
+
+					// Break if we have already found our previous block
+					if (prevBlock != null) {
+						break;
+					}
+				} else if (currBlock.getOffset() > end) {
+					/*
+					 * Move iterator back so we can insert right before
+					 * currBlock which has a larger offset
+					 */
+					blockIt.previous();
+					break;
+				}
+			}
+
+			if (prevBlock != null && nextBlock != null) {
+				// System.out.println("  Merging blocks");
+				freeBlocks.remove(nextBlock);
+				prevBlock.addToEnd(size + nextBlock.getSize());
+			} else if (prevBlock != null) {
+				// System.out.println("  Adding to end");
+				prevBlock.addToEnd(size);
+			} else if (nextBlock != null) {
+				// System.out.println("  Adding to start");
+				nextBlock.addToFront(size);
+			} else {
+				// System.out.println("  Creating new free block "+offset+"+"+size);
+				/*
+				 * If lists is empty, iterator will add to beginning. Else, we
+				 * are inserting before the bigger
+				 */
+				blockIt.add(new FreeBlock(size, offset));
+			}
+
+		}
+
+		/**
+		 * Return the byte offset in the file where the sequence will be stored
+		 * 
+		 * @param sequenceLength
+		 * @return
+		 */
+		public long allocateBlock(int blockSize) {
+			Iterator<FreeBlock> blockIt = freeBlocks.iterator();
+			FreeBlock block = null;
+			while (blockIt.hasNext()) {
+				block = blockIt.next();
+				if (block.getSize() == blockSize) {
+					/*
+					 * If sizes are exact matches, we consume this block
+					 */
+					blockIt.remove();
+					return block.getOffset();
+				} else if (block.getSize() > blockSize) {
+					long offset = block.getOffset();
+					block.takeFromFront(blockSize);
+					return offset;
+				}
+			}
+
+			/*
+			 * If we've gotten this far, we need to expand the file. Need to
+			 * return offset of EOF plus any extra space that was too small.
+			 * Check if the last free block is at end of file.
+			 * 
+			 * If block is null, there are no free blocks, so we store at end
+			 */
+			long eof = sequenceFile.length();
+			if (block != null && block.getEnd() >= eof) {
+				eof -= block.getSize(); // take this free block
+				blockIt.remove();
+			}
+			return eof;
+			/*
+			 * If we don't have a free space, we return a negative number
+			 */
+		}
+
+		/**
+		 * Represents a free area in the file
+		 * 
+		 * @author loganlinn
+		 * 
+		 */
+		private class FreeBlock {
+			private int size; // block size in bytes
+			private long offset; // location in file
+
+			public FreeBlock(int size, long offset) {
+				super();
+				this.size = size;
+				this.offset = offset;
+			}
+
+			/**
+			 * @return the size
+			 */
+			public int getSize() {
+				return size;
+			}
+
+			/**
+			 * @return the offset
+			 */
+			public long getOffset() {
+				return offset;
+			}
+
+			public long getEnd() {
+				return offset + size;
+			}
+
+			public void addToEnd(int step) {
+				size += step;
+			}
+
+			public void addToFront(int step) {
+				size += step;
+				offset -= step;
+			}
+
+			public void takeFromFront(int step) {
+				size -= step;
+				offset += step;
+			}
+
+			public String toString() {
+				return "Free: " + offset + "+" + size;
+			}
+		}
+
+		public void printFreeBlocks() {
+			for (FreeBlock fb : freeBlocks) {
+				System.out.println("  " + fb.toString());
+			}
+		}
+	}
+
 }
